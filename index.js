48e28b2f40f9f9ac7fe94c4b1a5a8d2a35e2f525
emerson4654-web <emerson4654@gmail.com>
2026-09-20 20:26:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,51 +2,52 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const { joinVoiceChannel } = require("@discordjs/voice");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildVoiceStates
-    ]
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent,
+GatewayIntentBits.GuildVoiceStates
+]
 });
 
 const VOICE_CHANNEL_ID = "1548062908557172786";
 
 client.once("ready", () => {
-    console.log("Bot online como " + client.user.tag);
+console.log("Bot online como " + client.user.tag);
 });
 
 client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
+if (message.author.bot) return;
 
-    if (message.content.toLowerCase() !== "!negas") return;
+if (message.content.toLowerCase() !== "!negas") return;  
 
-    try {
-        const channel = await client.channels.fetch(VOICE_CHANNEL_ID);
+try {  
+    const channel = await client.channels.fetch(VOICE_CHANNEL_ID);  
 
-        if (!channel) {
-            await message.reply("❌ Não encontrei essa call.");
-            return;
-        }
+    if (!channel) {  
+        await message.reply("❌ Não encontrei essa call.");  
+        return;  
+    }  
 
-        if (!channel.isVoiceBased()) {
-            await message.reply("❌ Esse ID não é um canal de voz.");
-            return;
-        }
+    if (!channel.isVoiceBased()) {  
+        await message.reply("❌ Esse ID não é um canal de voz.");  
+        return;  
+    }  
 
-        joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-            selfDeaf: false,
-            selfMute: false
-        });
+    joinVoiceChannel({  
+        channelId: channel.id,  
+        guildId: channel.guild.id,  
+        adapterCreator: channel.guild.voiceAdapterCreator,  
+        selfDeaf: false,  
+        selfMute: false  
+    });  
 
-        await message.reply("✅ Entrei na call!");
-    } catch (error) {
-        console.error(error);
-        await message.reply("❌ Não consegui entrar na call.");
-    }
+    await message.reply("✅ Entrei na call!");  
+} catch (error) {  
+    console.error(error);  
+    await message.reply("❌ Não consegui entrar na call.");  
+}
+
 });
 
-client.login(process.env.MTU1MTM2NzE4NzU2OTMxMTc0NA.G6g2QZ.4HUNpk4Y2ptGsQCiMKoVnSYtv0aPA8u87wfDq8;
+client.login(process.env.MTU1MTM2NzE4NzU2OTMxMTc0NA.G6g2QZ.4HUNpk4Y2ptGsQCiMKoVnSYtv0aPA8u87wfDq8);
